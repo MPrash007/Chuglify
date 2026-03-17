37e7133e7d2f1c9081a19d3536ddceded578a73b
Prakash <prash8264@gmail.com>
2026-03-17 22:45:03 +0530

    update profile endpoint added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser'
+
+
+
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
-import path from 'path';
 import { connectDB } from './lib/db.js';
 import { ENV } from './lib/env.js';
 
@@ -13,6 +17,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000; 
 
 app.use(express.json()); // for parsing application/json
+app.use(cookieParser())
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
